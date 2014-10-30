638633ba69bb7348b176412229ef64e0e4eec11a
Ravenwolfe <brian@tindomorgans.com>
2014-10-30 15:34:50 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace JustUOStartup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inputName = new System.Windows.Forms.TextBox();
+            this.ClientPath = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.results = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(70, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(495, 225);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // inputName
+            // 
+            this.inputName.Location = new System.Drawing.Point(106, 294);
+            this.inputName.Name = "inputName";
+            this.inputName.Size = new System.Drawing.Size(392, 20);
+            this.inputName.TabIndex = 1;
+            // 
+            // ClientPath
+            // 
+            this.ClientPath.AutoSize = true;
+            this.ClientPath.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientPath.Location = new System.Drawing.Point(120, 338);
+            this.ClientPath.Name = "ClientPath";
+            this.ClientPath.Size = new System.Drawing.Size(269, 15);
+            this.ClientPath.TabIndex = 3;
+            this.ClientPath.Text = "Please Browse to the location of your UO client:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(395, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // start
+            // 
+            this.start.AutoSize = true;
+            this.start.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(535, 314);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(87, 19);
+            this.start.TabIndex = 5;
+            this.start.Text = "Lets do this!";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Go!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // results
+            // 
+            this.results.Location = new System.Drawing.Point(106, 376);
+            this.results.Name = "results";
+            this.results.ReadOnly = true;
+            this.results.Size = new System.Drawing.Size(392, 20);
+            this.results.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Please type the name to be used for your shard in the box below:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(644, 422);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ClientPath);
+            this.Controls.Add(this.inputName);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "JustUO Setup";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox inputName;
+        private System.Windows.Forms.Label ClientPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label start;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace JustUOStartup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string clientpath = this.folderBrowserDialog1.SelectedPath;
+                string clientfile = clientpath + "\\client.exe";
+
+                if (!File.Exists(clientfile))
+                {
+                    MessageBox.Show("This folder does not appear to contain client files!");
+                }
+
+                else
+                    results.Text = clientpath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string _clientpath = results.Text;
+            string _shardname = inputName.Text;
+
+            if (String.IsNullOrEmpty(_shardname))
+            {
+                MessageBox.Show("You must type a shard name!");
+            }
+
+            else if (String.IsNullOrEmpty(_clientpath))
+            {
+                MessageBox.Show("You must select a path to your client files!");
+            }
+
+            else
+            {
+                if (File.Exists(@"startup.xml"))
+                    File.Delete(@"startup.xml");
+
+                using (XmlWriter writer = XmlWriter.Create("startup.xml"))
+                {
+                    writer.WriteStartElement("startupinfo");
+                    writer.WriteElementString("shardname", _shardname);
+                    writer.WriteElementString("clientpath", _clientpath);
+                    writer.WriteEndElement();
+                    writer.Flush();
+                }
+
+                MessageBox.Show("Initialization complete, we will now compile your server and start it!");
+
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = "Compile.WIN.bat";
+                proc.StartInfo.RedirectStandardError = false;
+                proc.StartInfo.RedirectStandardOutput = false;
+                proc.StartInfo.UseShellExecute = false;
+                proc.Start();
+
+                Application.Exit();
+             }
+        }
+    }
+}
